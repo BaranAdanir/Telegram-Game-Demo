690650fa92896a1d663bd34871534b0ff1a36958
Baran Adanır <baranadanir350@gmail.com>
2024-07-30 09:34:17 +0300

    [Added] New Libraries

--- Telegram/index.js
+++ Telegram/index.js
@@ -4,27 +4,44 @@ const bot = new TelegramBot(TOKEN, { polling: true });
 const gameName = "telegram_demo_game";
 const queries = {};
 
-bot.onText(/help/, (msg) => bot.sendMessage(msg.from.id, "Say /game if you want to play."));
-bot.onText(/start|game/, (msg) => bot.sendGame(msg.from.id, gameName));
+bot.onText(/help/, (msg) => {
+    console.log("Received help command");
+    bot.sendMessage(msg.from.id, "Type /start to play.");
+});
+
+bot.onText(/start/, (msg) => {
+    console.log("Received /start command");
+    bot.sendGame(msg.from.id, gameName, {
+        reply_markup: {
+            inline_keyboard: [[{
+                text: "Play Game",
+                callback_game: {
+                    short_name: gameName
+                }
+            }, {
+                text: "Help",
+                callback_data: "help"
+            }]]
+        }
+    });
+});
 
 bot.on("callback_query", (query) => {
-    if (query.game_short_name !== gameName) {
+    console.log("Received callback query:", query);
+    if (query.data === "help") {
+        bot.answerCallbackQuery(query.id);
+        bot.sendMessage(query.from.id, "This is a placeholder text for /help command.");
+    } else if (query.game_short_name !== gameName) {
         bot.answerCallbackQuery(query.id, {
             text: "Sorry, '" + query.game_short_name + "' is not available."
         });
     } else {
-        queries[query.id] = query;
-        let gameurl = "https://baranadanir.github.io/Telegram-Game-Demo/";
+        console.log("Starting game for query:", query);
+
         bot.answerCallbackQuery(query.id, {
-            url: gameurl
+            url: "https://baranadanir.github.io/Telegram-Game-Demo/"
         });
     }
 });
 
-bot.on("inline_query", (iq) => {
-    bot.answerInlineQuery(iq.id, [{
-        type: "game",
-        id: "0",
-        game_short_name: gameName
-    }]);
-});
+console.log("Bot is up and running...");
